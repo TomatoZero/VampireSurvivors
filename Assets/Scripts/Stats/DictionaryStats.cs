c7b82c2d729c942e2a21a129e74c02f0667d5362
TOMATO\chima <chima05.2015@gmail.com>
2023-09-02 13:59:17 +0300

    feat(game logic): create PlayerStats scriptable object

---
+++ Assets/Scripts/Stats/DictionaryStats.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+namespace Stats
+{
+    [Serializable]
+    public class Stat
+    {
+        [SerializeField] private Stats _stat;
+        [SerializeField] private int _value;
+    }
+}
---
+++ Assets/Scripts/Stats/ScriptableObjects/PlayerStatsData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Stats.ScriptableObjects
+{
+    [CreateAssetMenu(fileName = "PlayerStatsData", menuName = "ScriptableObject/Stats/Player", order = 0)]
+    public class PlayerStatsData : ScriptableObject
+    {
+        [FormerlySerializedAs("_playerStats")] [SerializeField] private List<Stat> _stats;
+
+        public List<Stat> Stats => _stats;
+    }
+}

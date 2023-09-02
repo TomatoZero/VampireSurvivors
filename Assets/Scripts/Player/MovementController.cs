ebdc8bfd10f5fa622da2b325ad7928782f973685
TOMATO\chima <chima05.2015@gmail.com>
2023-09-02 18:07:18 +0300

    feat(game logic): implement method UpdateStatsEventHandler for MovementController

--- Assets/Scripts/Player/MovementController.cs
+++ Assets/Scripts/Player/MovementController.cs
@@ -1,22 +1,33 @@
+using Stats.Instances;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
 
 namespace Player
 {
     public class MovementController : MonoBehaviour
     {
-        [SerializeField] private float _speed;
-
+        [FormerlySerializedAs("_speed")] [SerializeField] private float _defaultSpeed;
+        
         private Vector2 _moveDirection;
+        private float _speedPlus;
 
         private void FixedUpdate()
         {
-            transform.Translate(_moveDirection.normalized * (_speed * Time.fixedDeltaTime));
+            transform.Translate(_moveDirection.normalized * ((_defaultSpeed + _speedPlus) * Time.fixedDeltaTime));
         }
 
         public void MoveEventHandler(InputAction.CallbackContext context)
         {
             _moveDirection = context.ReadValue<Vector2>();
+        }
+
+        public void UpdateStatsEventHandler(PlayerStats newStats)
+        {
+            float speedStatPercent = newStats.GetStatByName(Stats.Stats.MoveSpeed).Value;
+            _speedPlus = (_defaultSpeed * speedStatPercent) / 100;
+            
+            Debug.Log($"sped plus {_speedPlus}");
         }
     }    
 }

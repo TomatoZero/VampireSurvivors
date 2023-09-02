e3f1b04e30ae4edb948acc61e05c3f6fab73c1f6
TOMATO\chima <chima05.2015@gmail.com>
2023-09-02 13:21:14 +0300

    feat(game logic): add Stats enum

---
+++ Assets/Scripts/Stats/Stats.cs
@@ -0,0 +1,25 @@
+﻿namespace Stats
+{
+    public enum Stats
+    {
+        MaxHealth,
+        Armor,
+        Speed,
+        Area,
+        Cooldown,
+        Revival,
+        Growth,
+        Curse,
+        Reroll,
+        Banish,
+        Recovery,
+        MoveSpeed,
+        Might,
+        Duration,
+        Amount,
+        Magnet,
+        Luck,
+        Greed,
+        Skip
+    }
+}

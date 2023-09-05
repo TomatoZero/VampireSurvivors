59bf33042890f7f20ff5486d5526e54d30beba12
TOMATO\chima <chima05.2015@gmail.com>
2023-09-05 19:40:27 +0300

    feat(game logic): create EnemyStatsinctance class

---
+++ Assets/Scripts/Stats/Instances/EnemyStatsInstance.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Stats.ScriptableObjects;
+
+namespace Stats.Instances
+{
+    public class EnemyStatsInstance : ObjectStatsInstance
+    {
+        public List<StatData> CurrentStats => _currentStats;
+
+        public EnemyStatsInstance(EnemyStatsData statsData) : base(statsData)
+        {
+        }
+    }
+}

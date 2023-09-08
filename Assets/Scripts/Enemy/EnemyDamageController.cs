8ca215ec22fcf5bc53421d44e327cf4fbd6f3da3
TOMATO\chima <chima05.2015@gmail.com>
2023-09-08 11:35:07 +0300

    feat(game logic): create EnemyDamageController

---
+++ Assets/Scripts/Enemy/EnemyDamageController.cs
@@ -0,0 +1,25 @@
+﻿using Interface;
+using Stats.Instances;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Enemy
+{
+    public class EnemyDamageController : MonoBehaviour, IDamageable, IUpdateStats
+    {
+        [SerializeField] private UnityEvent<float> _takeDamageEvent;
+        private float _armor;
+        
+        public void TakeDamage(float damage)
+        {
+            if(damage - _armor <= 0) return;
+            
+            _takeDamageEvent.Invoke(damage - _armor);
+        }
+
+        public void UpdateStatsEventHandler(ObjectStatsInstance newStatsInstance)
+        {
+            _armor = newStatsInstance.GetStatByName(Stats.Stats.Armor).Value;
+        }
+    }
+}

3cd50d9eaceff37e415e19a22de6e1c9a5e6425e
TOMATO\chima <chima05.2015@gmail.com>
2023-09-19 16:00:30 +0300

    feat(game logic): implement MeleeWeaponAreaController

--- Assets/Scripts/Weapons/Melee/Garlic/GarlicAreaController.cs
+++ Assets/Scripts/Weapons/Melee/Garlic/GarlicAreaController.cs
@@ -1,36 +1,26 @@
-﻿using Interface;
-using Stats.Instances;
+﻿using Stats.Instances;
 using UnityEngine;
-using UnityEngine.Events;
 
 namespace Weapons.Melee.Garlic
 {
-    public class GarlicAreaController : MonoBehaviour, IUpdateStats
+    public class GarlicAreaController : MeleeWeaponAreaController
     {
-        [SerializeField] private LayerMask _enemyAndWeapon;
-        [SerializeField] private UnityEvent<Collider2D[]> _hitEnemyEventHandler;
-        [SerializeField] private UnityEvent _startCountdownEventHandler;
-
-        private bool _canDamage;
-
         private Vector3 _scale;
-        private float _defaultArea;
-        private float _area;
 
         private void Awake()
         {
             _scale = transform.localScale;
-            _defaultArea = _scale.y;
-            _canDamage = true;
+            DefaultArea = _scale.y;
+            CanDamage = true;
         }
 
-        private void OnTriggerStay2D(Collider2D other)
+        protected override void OnTriggerStay2D(Collider2D other)
         {
-            if (!_canDamage) return;
+            if (!CanDamage) return;
 
             if (((1 << other.gameObject.layer) & _enemyAndWeapon) != 0)
             {
-                _canDamage = false;
+                CanDamage = false;
                 var enemyInside = ScanForEnemy();
                 Debug.Log($"enemyInside {enemyInside.Length}");
                 _hitEnemyEventHandler.Invoke(enemyInside);
@@ -38,41 +28,19 @@ namespace Weapons.Melee.Garlic
             }
         }
 
-        public void AllowDamageEventHandler()
-        {
-            _canDamage = true;
-        }
-
-        public void ForbidDamageEventHandler()
-        {
-            _canDamage = false;
-        }
-
-        public void SetupStatEventHandler(ObjectInstance newInstance)
+        public override void SetupStatEventHandler(ObjectInstance newInstance)
         {
         }
 
-        public void UpdateStatsEventHandler(ObjectInstance newInstance)
+        public override void UpdateStatsEventHandler(ObjectInstance newInstance)
         {
-            var weaponInstance = (WeaponInstance)newInstance;
-            SetStat(_defaultArea, weaponInstance.GetStatByName(Stats.Stats.Area).Value);
+            base.UpdateStatsEventHandler(newInstance);
+            transform.localScale += new Vector3(Area - DefaultArea, Area - DefaultArea, 0);
         }
 
-        private Collider2D[] ScanForEnemy()
+        protected override Collider2D[] ScanForEnemy()
         {
             return Physics2D.OverlapCircleAll(transform.position, _scale.y / 2, _enemyAndWeapon);
         }
-
-        private void SetStat(float defaultValue, float addPercent)
-        {
-            var addValue = (defaultValue * addPercent) / 100;
-            transform.localScale += new Vector3(addValue, addValue, 0);
-        }
-
-        // void OnDrawGizmosSelected()
-        // {
-        //     Gizmos.color = Color.yellow;
-        //     Gizmos.DrawSphere(transform.position, _scale.y);
-        // }
     }
 }
---
+++ Assets/Scripts/Weapons/Melee/MeleeWeaponAreaController.cs
@@ -0,0 +1,51 @@
+﻿using Interface;
+using Stats.Instances;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Weapons.Melee
+{
+    public abstract class MeleeWeaponAreaController : MonoBehaviour, IUpdateStats
+    {
+        [SerializeField] private protected LayerMask _enemyAndWeapon;
+        [SerializeField] private protected UnityEvent<Collider2D[]> _hitEnemyEventHandler;
+        [SerializeField] private protected UnityEvent _startCountdownEventHandler;
+
+        private protected bool CanDamage;
+
+        private protected float DefaultArea;
+        private protected float Area;
+
+        protected abstract void OnTriggerStay2D(Collider2D other);
+
+        public void AllowDamageEventHandler()
+        {
+            CanDamage = true;
+        }
+
+        public void ForbidDamageEventHandler()
+        {
+            CanDamage = false;
+        }
+
+        public virtual void SetupStatEventHandler(ObjectInstance newInstance)
+        {
+            var weaponInstance = (WeaponInstance)newInstance;
+            SetStat(ref Area, DefaultArea, weaponInstance.GetStatByName(Stats.Stats.Area).Value);
+        }
+
+        public virtual void UpdateStatsEventHandler(ObjectInstance newInstance)
+        {
+            var weaponInstance = (WeaponInstance)newInstance;
+            SetStat(ref Area, DefaultArea, weaponInstance.GetStatByName(Stats.Stats.Area).Value);
+        }
+
+        protected virtual void SetStat(ref float variable, float defaultValue, float addPercent)
+        {
+            var addValue = (defaultValue * addPercent) / 100;
+            variable = defaultValue + addValue;
+        }
+
+        protected abstract Collider2D[] ScanForEnemy();
+    }
+}

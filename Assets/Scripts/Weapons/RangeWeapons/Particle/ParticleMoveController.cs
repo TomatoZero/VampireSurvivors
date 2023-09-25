c299b86822243dc45078341bbcb4a8e1cc7327b1
TOMATO\chima <chima05.2015@gmail.com>
2023-09-25 12:22:05 +0300

    feat(game logic) create ParticleMoveController

--- Assets/Scripts/Weapons/RangeWeapons/Particle/ParticleMoveController.cs
+++ Assets/Scripts/Weapons/RangeWeapons/Particle/ParticleMoveController.cs
@@ -1,9 +1,14 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Weapons.RangeWeapons.Particle
 {
-    public class ParticleMoveController : MonoBehaviour
+    public abstract class ParticleMoveController : MonoBehaviour
     {
-        
+        private Vector2 _moveDirection;
+
+        private protected abstract void FixedUpdate();
+
+        public abstract Vector2 MoveTo(Vector2 direction);
     }
 }

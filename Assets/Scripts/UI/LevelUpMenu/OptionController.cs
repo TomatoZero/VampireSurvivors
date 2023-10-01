915efc1fdfecb26d9e08a54d782a5cfd202bbf90
TOMATO\chima <chima05.2015@gmail.com>
2023-10-01 16:13:55 +0300

    feat(ui): create OptionPrefab for choising buf whel level up

---
+++ Assets/Scripts/UI/LevelUpMenu/OptionController.cs
@@ -0,0 +1,38 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.LevelUpMenu
+{
+    public class OptionController : MonoBehaviour
+    {
+        [SerializeField] private Image _ico;
+        [SerializeField] private TMP_Text _name;
+        [SerializeField] private TMP_Text _level;
+        [SerializeField] private TMP_Text _description;
+
+        public Image Ico
+        {
+            get => _ico;
+            set => _ico = value;
+        }
+
+        public TMP_Text Name
+        {
+            get => _name;
+            set => _name = value;
+        }
+
+        public TMP_Text Level
+        {
+            get => _level;
+            set => _level = value;
+        }
+
+        public TMP_Text Description
+        {
+            get => _description;
+            set => _description = value;
+        }
+    }
+}

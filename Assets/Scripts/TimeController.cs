f0ae7fe085cab98f03f672633bedba767ce7f107
TOMATO\chima <chima05.2015@gmail.com>
2023-10-01 12:33:28 +0300

    feat(game logic): create TimeController

---
+++ Assets/Scripts/TimeController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class TimeController : MonoBehaviour
+    {
+        public void TurnOnTime()
+        {
+            if(Time.timeScale == 1)
+            {
+                Debug.LogWarning("TimeScale already set to 1");
+                return;
+            }
+
+            Time.timeScale = 1;
+        }
+        
+        public void TurnOffTime()
+        {
+            if(Time.timeScale == 0)
+            {
+                Debug.LogWarning("TimeScale already set to 0");
+                return;
+            }
+
+            Time.timeScale = 0;
+        }
+        
+    }
+}

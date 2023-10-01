0106765c04e1d37ba59d21f7c39f79fb3be1e290
TOMATO\chima <chima05.2015@gmail.com>
2023-10-01 16:43:01 +0300

    feat(ui logic): create ProgresionPanel class for interaction with Progresion panel

---
+++ Assets/Scripts/UI/LevelUpMenu/ProgressionPanel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace UI.LevelUpMenu
+{
+    public class ProgressionPanel : MonoBehaviour
+    {
+        [SerializeField] private List<OptionController> _options;
+    }
+}

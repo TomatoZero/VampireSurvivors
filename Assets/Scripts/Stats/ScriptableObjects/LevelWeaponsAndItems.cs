bc65e30069fc29fdea0b51f6b21a3537ade6b889
TOMATO\chima <chima05.2015@gmail.com>
2023-10-03 10:59:31 +0300

    feat(game logic): create ScriptableObject witch contain all weapon and item in level

---
+++ Assets/Scripts/Stats/ScriptableObjects/LevelWeaponsAndItems.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Stats.ScriptableObjects
+{
+    [CreateAssetMenu(fileName = "LevelWeaponsAndItems", menuName = "ScriptableObject/LevelWeaponsAndItems", order = 0)]
+    public class LevelWeaponsAndItems : ScriptableObject
+    {
+        [SerializeField] private List<WeaponStatsData> _weapons;
+        [SerializeField] private List<ObjectStatsData> _items;
+    }
+}
--- Assets/Scripts/UI/LevelUpMenu/ProgressionPanel.cs
+++ Assets/Scripts/UI/LevelUpMenu/ProgressionPanel.cs
@@ -5,5 +5,6 @@ namespace UI.LevelUpMenu
     public class ProgressionPanel : MonoBehaviour
     {
         [SerializeField] private BonusPanel _bonusPanel;
+        // [SerializeField] private 
     }
 }

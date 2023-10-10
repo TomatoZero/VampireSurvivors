75aed5e46186661c87e71edcedc8ee903dd4783b
TOMATO\chima <chima05.2015@gmail.com>
2023-10-10 14:42:04 +0300

    feat(game logic): implement gem spaner

---
+++ Assets/Scripts/Spawner/GemSpawner.cs
@@ -0,0 +1,35 @@
+﻿using PickUpItems.Gem;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class GemSpawner : MonoBehaviour
+    {
+        [SerializeField] private GameObject _gemPrefab;
+        
+        public delegate void Magnet();
+
+        public event Magnet MagnetEvent;
+
+        public void MagnetPickUp()
+        {
+            MagnetEvent.Invoke();
+        }
+
+        public void Spawn(Vector3 pos)
+        {
+            var instance = Instantiate(_gemPrefab, pos, quaternion.identity, transform);
+            
+            if (instance.TryGetComponent(out GemDataController gemData))
+            {
+                gemData.Setup(GetXpBonus());    
+            }
+        }
+
+        private int GetXpBonus()
+        {
+            return 5;
+        }
+    }
+}

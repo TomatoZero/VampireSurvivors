23de503d63d51a5d5e65ae6bfec1155d6ac0d7d2
TOMATO\chima <chima05.2015@gmail.com>
2023-10-12 12:13:26 +0300

    feat(ui logic): create prefab for display curent stat

---
+++ Assets/Scripts/UI/CurrentStatsMenu/StatDisplay.cs
@@ -0,0 +1,21 @@
+﻿using Stats;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DefaultNamespace.UI.CurrentStatsMenu
+{
+    public class StatDisplay : MonoBehaviour
+    {
+        [SerializeField] private Image _ico;
+        [SerializeField] private TMP_Text _statName;
+        [SerializeField] private TMP_Text _statValue;
+
+        public void Setup(Sprite ico, string statName, string statValue)
+        {
+            _ico.sprite = ico;
+            _statName.text = statName;
+            _statValue.text = statValue;
+        }
+    }
+}

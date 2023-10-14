2673d17844815a231d8e3cdb0d511173f81cd0c7
TOMATO\chima <chima05.2015@gmail.com>
2023-10-14 14:05:21 +0300

    feat(ui logic): add class for interact with Buff prefab

---
+++ Assets/Scripts/UI/HUD/BuffDisplayController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace UI.HUD
+{
+    public class BuffDisplayController : MonoBehaviour
+    {
+        [SerializeField] private Image _buffIco;
+        [SerializeField] private UnityEvent<float> _buffTimeChangeEvent;
+
+        public void Setup(Sprite ico)
+        {
+            _buffIco.sprite = ico;
+        }
+
+        public void UpdateLeftTime(float value)
+        {
+            _buffTimeChangeEvent.Invoke(value);
+        }
+    }
+}
--- Assets/Scripts/UI/HUD/ItemDisplayController.cs
+++ Assets/Scripts/UI/HUD/ItemDisplayController.cs
@@ -8,5 +8,16 @@ namespace UI.HUD
     {
         [SerializeField] private Image _itemIco;
         [SerializeField] private TMP_Text _level;
+
+        public void Setup(Sprite ico, int level)
+        {
+            _itemIco.sprite = ico;
+            _level.text = $"Level {level}";
+        }
+
+        public void UpdateLevel(int level)
+        {
+            _level.text = $"Level {level}";
+        }
     }
 }

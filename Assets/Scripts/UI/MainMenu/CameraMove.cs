0d08fdd95df17fb1aae9e4eea36fb647640db44b
TOMATO\chima <chima05.2015@gmail.com>
2023-10-14 19:47:33 +0300

    feat(ui): create start menu

---
+++ Assets/Scripts/UI/MainMenu/CameraMove.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace UI.MainMenu
+{
+    public class CameraMove : MonoBehaviour
+    {
+        [SerializeField] private Rigidbody2D _rigidbody;
+        [SerializeField] private float _speed;
+        [SerializeField] private Vector2 _moveDirection;
+
+        private void FixedUpdate()
+        {
+            var newPos = _rigidbody.position + _moveDirection.normalized * ((_speed) * Time.fixedDeltaTime);
+            _rigidbody.MovePosition(newPos);
+        }
+
+        private void OnCollisionStay2D(Collision2D other)
+        {
+            _moveDirection = GetMoveDirection();
+            Debug.Log(_moveDirection);
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            _moveDirection = GetMoveDirection();
+            Debug.Log(_moveDirection);
+        }
+
+        private Vector3 GetMoveDirection()
+        {
+            var newDirection = new Vector3(Random.Range(-1,1), Random.Range(-1,1));
+
+            if (newDirection.Equals(Vector2.zero))
+            {
+                return -_moveDirection;
+            }
+            
+            return newDirection;
+        }
+    }
+}

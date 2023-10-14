7e27f74ed930cbe058b410e6e1110c658fc7e23b
TOMATO\chima <chima05.2015@gmail.com>
2023-10-14 13:41:01 +0300

    feat(assets): import assets for UI, items, map, player, enemy

---
+++ Assets/Scripts/UI/HUD/ItemDisplayController.cs
@@ -0,0 +1,12 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.HUD
+{
+    public class ItemDisplayController : MonoBehaviour
+    {
+        [SerializeField] private Image _itemIco;
+        [SerializeField] private TMP_Text _level;
+    }
+}

b34b70ed4f1e0aba021645fad4a7440eab132adf
TOMATO\chima <chima05.2015@gmail.com>
2023-10-20 14:20:34 +0300

    feat(game logic):implement script LightningParticleEffectController

---
+++ Assets/Scripts/Weapons/Melee/LightningRing/LightningParticleEffectController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+
+namespace Weapons.Melee.LightningRing
+{
+    public class LightningParticleEffectController : MonoBehaviour
+    {
+        [SerializeField] private ParticleSystem _particleSystem;
+        
+        public event Action<LightningParticleEffectController> EffectEndEvent;
+
+        private void Awake()
+        {
+            var main = _particleSystem.main;
+            main.stopAction = ParticleSystemStopAction.Callback;
+        }
+
+        public void Set(Vector2 position)
+        {
+            transform.position = position;
+            TurnOn();
+        }
+
+        void OnParticleSystemStopped()
+        {
+            Debug.Log("System has stopped!");
+        }
+
+        private void TurnOn()
+        {
+            gameObject.SetActive(true);
+            _particleSystem.Play();
+        }
+
+        private void TurnOff()
+        {
+            gameObject.SetActive(false);
+            _particleSystem.Stop();
+        }
+    }
+}
--- Assets/Scripts/Weapons/Melee/LightningRing/LightningRingEffect.cs
+++ Assets/Scripts/Weapons/Melee/LightningRing/LightningRingEffect.cs
@@ -1,12 +1,44 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace Weapons.Melee.LightningRing
 {
     public class LightningRingEffect : MonoBehaviour
     {
+        [SerializeField] private GameObject _particleEffect;
+
+        private Queue<LightningParticleEffectController> _freeParticle;
+
+        private void Awake()
+        {
+            _freeParticle = new Queue<LightningParticleEffectController>();
+        }
+
         public void SpawnLightning(Vector2[] positions)
         {
-            
+            foreach (var position in positions)
+            {
+                var particle = GetParticle();
+                particle.Set(position);
+            }
+        }
+
+        private void SetFreeParticle(LightningParticleEffectController gameObject)
+        {
+            _freeParticle.Enqueue(gameObject);
+        }
+
+        private LightningParticleEffectController GetParticle()
+        {
+            if (_freeParticle.TryDequeue(out LightningParticleEffectController freeParticle))
+                return freeParticle;
+            else
+            {
+                var obj = Instantiate(_particleEffect, transform);
+                var particleEffect = obj.GetComponent<LightningParticleEffectController>();
+                particleEffect.EffectEndEvent += SetFreeParticle;
+                return particleEffect;
+            }
         }
     }
 }

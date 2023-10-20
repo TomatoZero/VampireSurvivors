2f455fe6452f72ed668b629b79ce4592ff7501f1
TOMATO\chima <chima05.2015@gmail.com>
2023-10-20 21:12:30 +0300

    feat(ui logic) import ui assets

--- Assets/Scripts/Stats/ScriptableObjects/ObjectStatsData.cs
+++ Assets/Scripts/Stats/ScriptableObjects/ObjectStatsData.cs
@@ -9,11 +9,13 @@ namespace Stats.ScriptableObjects
     {
         [SerializeField] private Sprite _ico;
         [SerializeField] private string _name;
+        [SerializeField] private string _description;
         [SerializeField] private List<StatData> _defaultStatsData;
         [SerializeField] private List<LevelUpBonuses> _levelUpBonuses;
 
         public Sprite Ico => _ico;
         public string Name => _name;
+        public string Description => _description;
         public List<StatData> DefaultStatsData => _defaultStatsData;
         public List<LevelUpBonuses> LevelUpBonuses => _levelUpBonuses;
         public int MaxLvl => _levelUpBonuses.Count + 1;
--- Assets/Scripts/UI/LevelUpMenu/OptionController.cs
+++ Assets/Scripts/UI/LevelUpMenu/OptionController.cs
@@ -8,11 +8,24 @@ namespace UI.LevelUpMenu
 {
     public class OptionController : MonoBehaviour
     {
+        [Header("Sprite for weapon")] [SerializeField]
+        private Sprite _weaponFrame;
+
+        [SerializeField] private Sprite _weaponBackground;
+
+        [Header("Sprite for Items")] [SerializeField]
+        private Sprite _itemFrame;
+
+        [SerializeField] private Sprite _itemBackground;
+
+        [Header("Component reference")]
         [SerializeField] private Image _ico;
         [SerializeField] private TMP_Text _name;
         [SerializeField] private TMP_Text _level;
         [SerializeField] private TMP_Text _description;
         [SerializeField] private Button _button;
+        [SerializeField] private Image _frame;
+        [SerializeField] private Image _background;
 
         private BonusData _data;
 
@@ -27,11 +40,31 @@ namespace UI.LevelUpMenu
             _ico.sprite = data.StatsData.Ico;
             _name.text = data.StatsData.Name;
             _level.text = $"New level: {data.Level}";
-            
+
+            SetFrameAndBackground(data);
+            SetDescription(data);
+        }
+
+        private void SetFrameAndBackground(BonusData data)
+        {
+            if (data.IsWeapon)
+            {
+                _frame.sprite = _weaponFrame;
+                _background.sprite = _weaponBackground;
+            }
+            else
+            {
+                _frame.sprite = _itemFrame;
+                _background.sprite = _itemBackground;
+            }
+        }
+
+        private void SetDescription(BonusData data)
+        {
             if (data.Level == 0)
             {
                 _level.text = "";
-                _description.text = $"New weapon {data.StatsData.Name}";
+                _description.text = $"New weapon {data.StatsData.Name} {data.StatsData.Description}";
             }
             else _description.text = ConvertStatDataToDescription(data.StatsData.LevelUpBonuses[data.Level - 2]);
         }

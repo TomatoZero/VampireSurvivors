f4568da3af4cb9f0b530921f7070f183f1b9228b
Kostiuk Bohdan <kostyuk.bogdan.b@gmail.com>
2024-01-11 17:55:09 +0200

    feat(enemy logic): implement IState interface and EnemyStateMachine

---
+++ Assets/Scripts/Enemy/Rework/EnemyStateMachine.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Interface;
+
+namespace Enemy.Rework
+{
+    [Serializable]
+    public class EnemyStateMachine
+    {
+        private IState _currentState;
+
+        public void Initialize(IState startState)
+        {
+            _currentState = startState;
+            startState.Enter();
+        }
+
+        public void TransitionTo(IState nextState)
+        {
+            _currentState.Exit();
+            _currentState = nextState;
+            _currentState.Enter();
+        }
+
+        public void Update()
+        {
+            if(_currentState is not null)
+                _currentState.Update();
+        }
+    }
+}
---
+++ Assets/Scripts/Interface/IState.cs
@@ -0,0 +1,9 @@
+﻿namespace Interface
+{
+    public interface IState
+    {
+        public void Enter();
+        public void Update();
+        public void Exit();
+    }
+}

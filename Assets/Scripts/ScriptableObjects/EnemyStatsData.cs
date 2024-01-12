f8eaf80a3461884dfd397f11c73bdea99c1c18ee
Kostiuk Bohdan <kostyuk.bogdan.b@gmail.com>
2024-01-12 12:08:07 +0200

    fix: regenerate project file and remove editor error

--- Assets/Scripts/ScriptableObjects/EnemyStatsData.cs
+++ Assets/Scripts/ScriptableObjects/EnemyStatsData.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using UnityEditor.Animations;
 using UnityEngine;
 
 namespace ScriptableObjects
@@ -6,8 +7,8 @@ namespace ScriptableObjects
     [CreateAssetMenu(fileName = "EnemyStatData", menuName = "ScriptableObject/Stats/EnemyRework", order = 0)]
     public class EnemyStatsData : ObjectStatsData
     {
-        // [SerializeField] private List<AnimatorController> _levelEvolution;
-        //
-        // public List<AnimatorController> LevelEvolution => _levelEvolution;
+        [SerializeField] private List<AnimatorController> _levelEvolution;
+
+        public List<AnimatorController> LevelEvolution => _levelEvolution;
     }
 }

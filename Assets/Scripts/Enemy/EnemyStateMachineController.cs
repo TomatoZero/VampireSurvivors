c3de6e8d172fc6c07ebd822bfe99191ad9bff2b1
Kostiuk Bohdan <kostyuk.bogdan.b@gmail.com>
2024-02-16 12:57:52 +0200

    feat: add field _buffController, and property BuffController

--- Assets/Scripts/Enemy/EnemyStateMachineController.cs
+++ Assets/Scripts/Enemy/EnemyStateMachineController.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using Controllers;
 using StateMachine;
 using UnityEngine;
 
@@ -7,13 +8,20 @@ namespace Enemy
     public abstract class EnemyStateMachineController : MonoBehaviour
     {
         [SerializeField] private EnemyMovementController _movementController;
+        [SerializeField] private BuffController _buffController;
 
         public EnemyMovementController MovementController
         {
             get => _movementController;
             protected set => _movementController = value;
         }
-        
+
+        public BuffController BuffController
+        {
+            get => _buffController;
+            protected set => _buffController = value;
+        }
+
         protected EnemyStateMachine StateMachine { get; set; }
         protected List<IState> States { get; set; }
 
